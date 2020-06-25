9ea1a379b4554b88757a5cbb175d4952e7ba7961
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2020-06-24 21:10:22 -0300

    Second class: enemie, collision and jump

--- scripts/game/scenery.js
+++ scripts/game/scenery.js
@@ -2,25 +2,25 @@ class Scenery {
   constructor(image, velocity) {
     this.image = image;
     this.velocity = velocity;
-    this.image1Position = 0;
-    this.image2Position = width;
+    this.firstImagePosition = 0;
+    this.secondImagePosition = width;
   }
   
   show() {
-    image(this.image, this.image1Position, 0, width, height);
-    image(this.image, this.image2Position, 0, width, height);
+    image(this.image, this.firstImagePosition, 0, width, height);
+    image(this.image, this.secondImagePosition, 0, width, height);
   }
   
   move() {
-    this.image1Position -= this.velocity;
-    this.image2Position -= this.velocity;
+    this.firstImagePosition -= this.velocity;
+    this.secondImagePosition -= this.velocity;
     
-    if (this.image1Position < -width) {
-      this.image1Position = width;
+    if (this.firstImagePosition < (- width)) {
+      this.firstImagePosition = width;
     }
     
-    if (this.image2Position < -width) {
-      this.image2Position = width;
+    if (this.secondImagePosition < (- width)) {
+      this.secondImagePosition = width;
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,55 @@
 let sceneryImage;
 let characterImage;
+let enemieImage;;
+
 let scenery;
 let character;
+let enemie;
+
 let gameSound;
+let jumpSound;
 
 function preload() {
   sceneryImage = loadImage('images/scenery/forest.png');
   characterImage = loadImage('images/character/running.png');
+  enemieImage = loadImage('images/enemies/drop.png');
+
   gameSound = loadSound('sounds/gameSoundtrack.mp3');
+  jumpSound = loadSound('sounds/jumpingSound.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenery = new Scenery(sceneryImage, 3);
-  character = new Character(characterImage, 220, 270, 16);
+  character = new Character(
+    characterImage,
+    4,
+    4,
+    110,
+    135,
+    50,
+    220,
+    270
+  );
+  enemie = new Enemie(
+    enemieImage,
+    4,
+    7,
+    52,
+    52,
+    width,
+    104,
+    104
+  );
   frameRate(40);
   gameSound.loop();
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    character.jump();
+    jumpSound.play();
+  }
 }
 
 function draw() {
@@ -23,4 +57,13 @@ function draw() {
   scenery.move();
 
   character.show();
+  character.applyGravity();
+
+  enemie.show();
+  enemie.move();
+
+  if (character.checkCollision(enemie)) {
+    console.log('sim')
+    noLoop()
+  }
 }
